d474f0062d79076d0123bdaff8ffbd3a5956e228
bertrand.cerot <bertrand.cerot@cgi.com>
2019-05-24 16:40:56 +0200

    Technicien à faire Manager ok (sans gestion exceptions) Commercial (en cours)

--- src/main/java/com/ipiecoles/java/java230/MyRunner.java
+++ src/main/java/com/ipiecoles/java/java230/MyRunner.java
@@ -31,18 +31,21 @@ public class MyRunner implements CommandLineRunner {
     private static final String REGEX_NOM = ".*";
     private static final String REGEX_PRENOM = ".*";
     private static final String REGEX_SALAIRE = "^[0-9]+\\.[0-9]{0,2}$";
-    //private static final String REGEX_SALAIRE = "^[0-9]+\\.[0-9]{0,2}$";
+    // *** Description du REGEX_SALAIRE ***
     // Une REGEX est toujours une String.
     // "^" sert à dire qu'on commence notre expression régulière
     // "[0-9]" désigne tous les chiffres que l'on va pouvoir utiliser
     // "+" veut dire que l'on veut au moins un chiffre
     // "\\" est utilisé pour échapper un caractère (ici le point)
+    // "?" sert à dire 0 ou une fois "l'instruction" précédente.
     // {0,2} veut dire que l'on veut uniquement des chiffres compris entre 0 et 2 (bien séparer par une virgule)
     // "$" sert à dire que c'est la fin de notre expression régulière
     private static final int NB_CHAMPS_MANAGER = 5;
     private static final int NB_CHAMPS_TECHNICIEN = 7;
     private static final String REGEX_MATRICULE_MANAGER = "^M[0-9]{5}$";
     private static final int NB_CHAMPS_COMMERCIAL = 7;
+    private static final String REGEX_CA = "^[0-9]+\\.[0-9]?$";
+    private static final String REGEX_PERFOMANCE = "^[0-9]+$";
 
     @Autowired
     private EmployeRepository employeRepository;
@@ -128,15 +131,15 @@ public class MyRunner implements CommandLineRunner {
             throw new BatchException("La longueur du champs commercial :" + commercialFields + " n'est pas bonne");
         }
 
-        if (commercialFields[0].matches(REGEX_MATRICULE)){
+        if (!commercialFields[0].matches(REGEX_MATRICULE)){
             throw new BatchException("Le matricule saisi ne répond pas aux conditions demandées");
         }
 
-        if (commercialFields[1].matches(REGEX_NOM)){
+        if (!commercialFields[1].matches(REGEX_NOM)){
             throw new BatchException("Le nom saisi ne répond pas aux conditions demandées");
         }
 
-        if (commercialFields[2].matches(REGEX_PRENOM)){
+        if (!commercialFields[2].matches(REGEX_PRENOM)){
             throw new BatchException("Le prénom saisi ne répond pas aux conditions demandées");
         }
 
@@ -148,15 +151,37 @@ public class MyRunner implements CommandLineRunner {
                 throw new BatchException( commercialFields[3] + " ne respecte pas le format de date dd/MM/yyyy");
             }
 
+        if (!commercialFields[4].matches(REGEX_SALAIRE)){
+            throw new BatchException("Le nombre indiqué n'est pas valide pour un salaire");
+        }
+
+        /* *** AUTRE POSSIBILITE AVEC UN TRY CATCH ***
         try{
-            float salaire = Float.parseFloat(commercialFields[4]);
+            double salaire = Double.parseDouble(commercialFields[4]);
         }
         catch (Exception e){
 
             throw new BatchException(commercialFields[4] + " n'est pas un nombre valide pour un salaire");
         }
+        */
 
+        if (!commercialFields[5].matches(REGEX_CA)){
+            throw new BatchException("Le nombre indiqué n'est pas valide pour un chiffre d'affaires");
+        }
 
+        /*
+        try{
+            float chiffreAffaires = Float.parseFloat(commercialFields[5]);
+        }
+        catch (Exception e){
+
+            throw new BatchException("Le chiffre d'affaire du commercial est incorrect : " + commercialFields[5]);
+        }
+        */
+
+        if (!commercialFields[6].matches(REGEX_PERFOMANCE)){
+            throw new BatchException("La performance du commercial est incorrecte : " + commercialFields[6]);
+        }
 
 
             Commercial c = new Commercial();
@@ -169,14 +194,44 @@ public class MyRunner implements CommandLineRunner {
      * @throws BatchException s'il y a un problème sur cette ligne
      */
     private void processManager(String ligneManager) throws BatchException {
-        //TODO
         String[] managerFields = ligneManager.split(",");
-        //Contrôle le matricule
-        managerFields[0].matches(REGEX_MATRICULE);
+
+        if (managerFields.length != NB_CHAMPS_MANAGER){
+            throw new BatchException("Le nombre de champs est incorrect");
+        }
+
+        if (!managerFields[0].matches(REGEX_MATRICULE)){
+            throw new BatchException("Le matricule ne respecte pas les règles");
+        }
+
+        if (!managerFields[1].matches(REGEX_NOM)){
+            throw new BatchException("Le nom ne respecte pas les règles");
+        }
+
+        if (!managerFields[2].matches(REGEX_PRENOM)){
+            throw new BatchException("Le prénom ne respecte pas les règles");
+        }
+
+        try{
+            LocalDate formatDate = DateTimeFormat.forPattern("dd/MM/yyyy").parseLocalDate(managerFields[3]);
+        }
+        catch (Exception e) {
+
+            throw new BatchException( managerFields[3] + " ne respecte pas le format de date dd/MM/yyyy");
+        }
+
+        try {
+            float salaire = Float.parseFloat(managerFields[4]);
+        }
+        catch (Exception e){
+            throw new BatchException( managerFields[4] + " n'est pas un format valide de salaire");
+        }
 
         Manager m = new Manager();
         employes.add(m);
+
     }
+
 
     /**
      * Méthode qui crée un Technicien à partir d'une ligne contenant les informations d'un technicien et l'ajoute dans la liste globale des employés
